10f7a8707978516d00fbb02f4d50df77d943055d
Eric-Bumaro <75790518+Eric-Bumaro@users.noreply.github.com>
2021-07-11 23:50:16 +0800

    For user register

--- miniproject/questionRecord/admin.py
+++ miniproject/questionRecord/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.register(Question)
-admin.register(Wrong)
+# admin.register(Question)
+# admin.register(Wrong)
