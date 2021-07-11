5c17f8930204c0dc177c7032de51147f1805bee7
Eric-Bumaro <75790518+Eric-Bumaro@users.noreply.github.com>
2021-07-11 23:53:46 +0800

    For user register

--- miniproject/miniproject/urls.py
+++ miniproject/miniproject/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from . import questionRecord
+# from . import questionRecord
 
 urlpatterns = [
     path('admin/', admin.site.urls),
