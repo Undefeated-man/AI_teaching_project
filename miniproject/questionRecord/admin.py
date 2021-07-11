972b48cd4b6bc1e55be282bc3871d07722fd61ce
Eric-Bumaro <75790518+Eric-Bumaro@users.noreply.github.com>
2021-07-11 23:57:14 +0800

    For user register

--- miniproject/questionRecord/admin.py
+++ miniproject/questionRecord/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-# admin.register(Question)
-# admin.register(Wrong)
+admin.register(Concept)
+admin.register(SubConcept)
+admin.register(Unit)
+admin.register(Example)
+admin.register(Wrong)
+admin.register(Level2)
+admin.register(Level3)
+admin.register(CommonUser)
+admin.register(Group)
+
