0cb195b5cf752d21b30553f1ba441260e191f4c2
Eric-Bumaro <75790518+Eric-Bumaro@users.noreply.github.com>
2021-07-28 22:37:22 +0800

    Collect

--- miniproject/questionRecord/views.py
+++ miniproject/questionRecord/views.py
@@ -187,9 +187,9 @@ def getNotesCollection(request):
             example = eval(i.level).objects.get(questionID=i.questionID).example
             if collectedDict.get(example.unit.unitName,None) is None:
                 collectedDict[example.unit.unitName]={}
-            if collectedDict.get(i.level,None) is None:
+            if collectedDict[example.unit.unitName].get(i.level,None) is None:
                 collectedDict[example.unit.unitName][i.level]=[]
-            collectedDict[example.unit.unitName][i.level].add(i.questionID)
+            collectedDict[example.unit.unitName][i.level].append(i.questionID)
         return JsonResponse({"state": "success", "collectedQuestion": collectedDict})
     except Exception as e:
         return JsonResponse({'state': 'fail', "error": e.__str__()})
