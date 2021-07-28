43a6441fad2118ccfccceb29cb115950bad7aab7
Xu Yinong <liafonx@outlook.com>
2021-07-29 03:24:07 +0800

    Update views.py

--- miniproject/questionRecord/views.py
+++ miniproject/questionRecord/views.py
@@ -187,7 +187,7 @@ def getNotesCollection(request):
         collectedDict[lect][i.level].append({"Question": eval(i.level).objects.get(questionID=i.questionID).question,
                                              "Answer": answer})
         print(collectedDict)
-    return JsonResponse({"state": "success", "collectedQuestion": collectedDict})
+    return JsonResponse({"state": "success", "collectedQuestion": sorted(collectedDict)})
 
 
 # except Exception as e:
