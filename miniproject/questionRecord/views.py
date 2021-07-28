254a1d8a5fe5744cf13ae8ed12986b489e475934
Xu Yinong <liafonx@outlook.com>
2021-07-28 23:51:07 +0800

    update collection format

--- miniproject/questionRecord/views.py
+++ miniproject/questionRecord/views.py
@@ -183,8 +183,8 @@ def getNotesCollection(request):
                 answer=example.concept.conceptName
             else:
                 answer=example.meaning
-            lect = example.unit.unitName
-            collectedDict[lect.replace('Lecture  ', 'LECT')][i.level].append({"Question":eval(i.level).objects.get(questionID=i.questionID).question,
+            lect = example.unit.unitName.replace('Lecture  ', 'LECT')
+            collectedDict[lect][i.level].append({"Question":eval(i.level).objects.get(questionID=i.questionID).question,
                                                                   "Answer":answer})
         return JsonResponse({"state": "success", "collectedQuestion": collectedDict})
     except Exception as e:
