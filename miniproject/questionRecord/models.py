bdb2c9dc7230b89841285361fedfec09947d4639
Xu Yinong <liafonx@outlook.com>
2021-07-31 00:24:31 +0800

    update

--- miniproject/questionRecord/models.py
+++ miniproject/questionRecord/models.py
@@ -155,7 +155,6 @@ class CommonUser(models.Model):
         , (3,"Level3"),(4,"Level4")],
                              default="Level1")
     imageLocation=models.TextField()
-    conSign=models.IntegerField(null=False, blank=False,default=0)
     continueCheckDays = models.IntegerField(null=False, blank=False, default=0)
     lastCheckDate = models.DateField()
     level2Lock = models.BooleanField(null=False, default=True)
