e12790ab0824d1959cfe2ac4eef51b1c170667d6
XUYinong <liafonx@outlook.com>
2021-09-02 23:07:09 +0800

    update

--- miniproject/miniproject/settings.py
+++ miniproject/miniproject/settings.py
@@ -121,6 +121,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static'), )
 
 # 图片储存根路径
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
