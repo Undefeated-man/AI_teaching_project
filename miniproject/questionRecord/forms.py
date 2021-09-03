9537a439c2d614a5da4793e08a5a6c837e082fb4
XUYinong <liafonx@outlook.com>
2021-09-03 12:39:38 +0800

    update

--- miniproject/questionRecord/forms.py
+++ miniproject/questionRecord/forms.py
@@ -4,5 +4,5 @@ import django.core.validators as validators
 
 class FileFieldForm(forms.Form):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True, 'class': "bg-info", 'id': 'fileinp',
-                                                                        'accept': '.xlsx', 'title': '', 'value': 'Step 1 Select Lecture Files'}),
+                                                                        'accept': '.xlsx', 'title': ''}),
                                  validators=[validators.FileExtensionValidator(['xlsx'])])
