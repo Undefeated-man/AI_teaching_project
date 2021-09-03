0f6c354048ddd4051aac4c9943f93b5aa231bf0e
XUYinong <liafonx@outlook.com>
2021-09-03 11:34:26 +0800

    Update forms.py

--- miniproject/questionRecord/forms.py
+++ miniproject/questionRecord/forms.py
@@ -4,5 +4,6 @@ import django.core.validators as validators
 
 class FileFieldForm(forms.Form):
     file_field = forms.FileField(label='选择多个文件', help_text='提示：注意*****',
-                                 widget=forms.ClearableFileInput(attrs={'multiple': True, 'class': "bg-info"}),
+                                 widget=forms.ClearableFileInput(attrs={'multiple': True, 'class': "bg-info",
+                                                                        'accept': '.xlsx'}),
                                  validators=[validators.FileExtensionValidator(['xlsx'], message='必须是xlsx文件')])
