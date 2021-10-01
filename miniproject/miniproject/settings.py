630f1ae0279e732fbdd2f095622f7e68f624bcab
XUYinong <liafonx@outlook.com>
2021-10-01 16:52:15 +0800

    Update settings.py

--- miniproject/miniproject/settings.py
+++ miniproject/miniproject/settings.py
@@ -134,7 +134,7 @@ DATE_INPUT_FORMATS = ['%Y-%m-%d']
 
 CRONJOBS = [
     # 表示每天2：01执行
-    ('50 16 * * *', 'questionRecord.views.SetDailyRank', '>/home/test.log')
+    ('0 4 * * *', 'questionRecord.views.SetDailyRank', '>/home/test.log')
 ]
 
 CRONTAB_COMMAND_SUFFIX = '2>&1'
